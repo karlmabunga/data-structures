2f4eef34c1f1a061218990974472222a4c6bed85
Karl Mabunga <karlmabunga@yahoo.com>
2020-08-19 16:32:28 -0700

    Able to complete stack functional and queue functional. Made significant progress on queue functional-shared.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,44 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // create obj to store queue
+  var someInstance = {};
+
+  // create var for storage
+  someInstance.storage = {};
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+  // return queue
+  return someInstance;
 };
+
+// create var for position of values
+var position = 0;
+var deposition = 0;
 
 var queueMethods = {};
 
+// add enqueue property function to queueMethods
+queueMethods.enqueue = function(value) {
+  this.storage[position] = value;
+  position++;
+};
+// add dequeue property function to queueMethods
+queueMethods.dequeue = function() {
+  // take our the first thing in enqueue
+  var tempHold = this.storage[position];
+  console.log(this.storage[deposition]);
+  delete this.storage[position];
+  // position++;
+  position--;
+  return tempHold;
+};
+// add size property function to queueMethods
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
+
+
+console.log(Queue());
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,32 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // create var for position
+  var position = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // increment position
+    position++;
+    // store position in storage with value of value
+    storage[position] = value;
   };
 
   someInstance.pop = function() {
+    // if size is not zero
+    if (position > 0) {
+      // create a temporary hold var for return value
+      var tempHold = storage[position];
+      // delete storage value to fix size
+      delete storage[position];
+      position--;
+      return tempHold;
+    }
   };
 
   someInstance.size = function() {
+    // return size of keys in the obj
+    return position;
   };
 
   return someInstance;
