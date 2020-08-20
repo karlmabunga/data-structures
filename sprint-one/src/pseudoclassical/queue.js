0a3627e93a7ae53a2b8bb9235dad49f526fde3e2
Karl Mabunga <karlmabunga@yahoo.com>
2020-08-20 16:18:20 -0700

    Completed psuedoclassical stack and queue.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,41 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // assign this storage to be an empty obj
+  this.storage = {};
+  // assign this position to be 0
+  this.position = 0;
+  // assign this depostion to be 0
+  this.deposition = 0;
+
+};
+
+//  create queue prototype method enqueue
+Queue.prototype.enqueue = function(value) {
+  // increment the position
+  this.position++;
+  // assign this storage at position to be the value
+  this.storage[this.position] = value;
+};
+
+//  create queue prototype method dequeue
+Queue.prototype.dequeue = function() {
+  // check if the posititon is greater than 0
+  if (this.size() > 0) {
+    // increment the depostion
+    this.deposition++;
+    // create a temp var to hold the value to dequeue
+    var tempHold = this.storage[this.deposition];
+    // delete the dequeue val
+    delete this.storage[this.deposition];
+    // return the temp var
+    return tempHold;
+  }
 };
 
 
+//  create queue prototype method pop
+Queue.prototype.size = function() {
+  // return the position(end) from the deposition(start)
+  return this.position - this. deposition;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // assign this to have a storage obj
+  this.storage = {};
+  // assign this to have a postion tracker
+  this.position = 0;
 };
 
 
+// assign stack.prototype to have the method push
+Stack.prototype.push = function(value) {
+  //   increment position
+  this.position++;
+  //   asssign position to take in value
+  this.storage[this.position] = value;
+};
+
+
+// assign stack.prototype to have the method pop
+Stack.prototype.pop = function() {
+  //   check if position is greater than 0
+  if (this.position > 0) {
+    //   create temp var to hold return data
+    var tempHold = this.storage[this.position];
+    //   delete this storage at position
+    delete this.storage[this.position];
+    //decrement position
+    this.position--;
+    // return temp var
+    return tempHold;
+  }
+};
+
+
+// assign stack.prototype to have the method size
+Stack.prototype.size = function() {
+  //   return position
+  return this.position;
+};
