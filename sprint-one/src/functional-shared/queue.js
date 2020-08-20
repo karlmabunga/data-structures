93672f8b5126d7563dcdf3d7bf9ae0ff0e71e83a
Karl Mabunga <karlmabunga@yahoo.com>
2020-08-20 10:37:31 -0700

    Completed function-shared queue. Fixed dequeue and size

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,44 +1,42 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // create var for position of values
+  var position = 0;
+  var deposition = 0;
   // create obj to store queue
   var someInstance = {};
 
+  someInstance.position = position;
+  someInstance.deposition = deposition;
   // create var for storage
   someInstance.storage = {};
-  someInstance.enqueue = queueMethods.enqueue;
-  someInstance.dequeue = queueMethods.dequeue;
-  someInstance.size = queueMethods.size;
+  _.extend(someInstance, queueMethods);
   // return queue
   return someInstance;
 };
 
-// create var for position of values
-var position = 0;
-var deposition = 0;
 
 var queueMethods = {};
 
 // add enqueue property function to queueMethods
 queueMethods.enqueue = function(value) {
-  this.storage[position] = value;
-  position++;
+  this.position++;
+  this.storage[this.position] = value;
 };
 // add dequeue property function to queueMethods
 queueMethods.dequeue = function() {
   // take our the first thing in enqueue
-  var tempHold = this.storage[position];
-  console.log(this.storage[deposition]);
-  delete this.storage[position];
-  // position++;
-  position--;
-  return tempHold;
+  if (this.size() > 0) {
+    this.deposition++;
+    var tempHold = this.storage[this.deposition];
+    delete this.storage[this.deposition];
+    // position++;
+    return tempHold;
+  }
 };
 // add size property function to queueMethods
 queueMethods.size = function() {
-  return Object.keys(this.storage).length;
+  return this.position - this.deposition;
 };
 
-
-
-console.log(Queue());
