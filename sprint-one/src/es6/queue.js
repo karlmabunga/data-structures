c1f592f77dcb220e49bd21c608c6f7661b0fafba
Karl Mabunga <karlmabunga@yahoo.com>
2020-08-20 17:28:29 -0700

    completed the minimum requirements for sprint 1 and gave stack and queue passing all test for ES6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,42 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    // this storage will be an empty obj
+    this.storage = {};
+    // this position will be 0
+    this.position = 0;
+    // this deposition will be 0
+    this.deposition = 0;
   }
 
+  // create method for enqueue
+  enqueue(value) {
+    // increment the position
+    this.position++;
+    // storage at position is value
+    this.storage[this.position] = value;
+  }
+
+
+  // create method for dequeue
+  dequeue() {
+    // check is size is greater than zero
+    if (this.size() > 0) {
+      // incrememnt depostion
+      this.deposition++;
+      // create temp var to hold storage at deposition
+      var tempHold = this.storage[this.deposition];
+      // delete storage at deposition
+      delete this.storage[this.deposition];
+      // return tempHold
+      return tempHold;
+    }
+  }
+
+
+  // create method for size
+  size() {
+    // return position minus deposition
+    return this.position - this.deposition;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,39 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    // assign this stack to have storage which will be empty obj
+    this.storage = {};
+    // assign this stack to have a position set to 0
+    this.position = 0;
   }
 
+  // declare function push
+  push(value) {
+    // increment position
+    this.position++;
+    // store value at storage at position
+    this.storage[this.position] = value;
+  }
+
+  // declare function pop
+  pop() {
+    // if position is greater than 0
+    if (this.position > 0) {
+      // make var temphold to be assigned to storage at position
+      var tempHold = this.storage[this.position];
+      // delete storage at position
+      delete this.storage[this.position];
+      // decrement postion
+      this.position--;
+      // return temphold
+      return tempHold;
+    }
+  }
+
+
+  // declare  size
+  size() {
+  // return position
+    return this.position;
+  }
 }
