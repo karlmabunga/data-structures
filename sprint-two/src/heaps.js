771dbfaf28df29f49b55a87327a964faee284d4c
Karl Mabunga <karlmabunga@yahoo.com>
2021-02-24 13:21:16 -0800

    Worked on methods extract max and sinkdown for heaps

--- sprint-two/src/heaps.js
+++ sprint-two/src/heaps.js
@@ -22,9 +22,49 @@ class MaxBinaryHeap {
       idx = parentIdx;
     }
   }
+  extractMax() {
+    let max = this.values[0];
+    let end = this.values.pop();
+    // Edge case for only one element in the heap
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+
+  }
+  sinkDown() {
+    let idx = 0;
+    let length = this.values.length;
+    let element = this.values[idx];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (swap === null && rightChild > element || swap !== null && rightChild > leftChild) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) {
+        break;
+      }
+      this.values[idx] = this.values[swap];
+      idx = swap;
+    }
+  }
 }
 
 
 let heap = new MaxBinaryHeap([41, 39, 33, 18, 27, 12]);
 heap.insert(55);
+console.log(heap.extractMax());
 console.log(heap);
